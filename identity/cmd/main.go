26362a39be531357d573c7333d150d28a4665e04
조석규 <ezaurum@gmail.com>
2018-04-03 02:47:37 +0900

    Move initialize DB to runner
    러너 쪽에 공통 코드를 보냄

--- identity/cmd/main.go
+++ identity/cmd/main.go
@@ -5,11 +5,6 @@ import (
 	"github.com/ezaurum/cthulthu/runner"
 	"github.com/ezaurum/cthulthu/session"
 	"github.com/gin-gonic/gin"
-	"github.com/ezaurum/cthulthu/authenticator"
-	"github.com/ezaurum/cthulthu"
-	"github.com/ezaurum/cthulthu/authorizer"
-	"github.com/ezaurum/cthulthu/helper"
-	"github.com/ezaurum/cthulthu/render"
 	"github.com/ezaurum/cthulthu/config"
 	"github.com/ezaurum/cthulthu/route"
 )
@@ -31,13 +26,12 @@ func makeConfig() *config.Config {
 	}
 
 	cf.Initialize = func(engine *gin.Engine) {
-		initialize(&cf, engine)
+		initialize(&cf)
 	}
 	return &cf
 }
 
-func initialize(config *config.Config,
-	r *gin.Engine) {
+func initialize(config *config.Config) {
 
 	if nil == config.DBManager {
 		panic("DB manager cannot be nil")
@@ -45,31 +39,10 @@ func initialize(config *config.Config,
 
 	manager := config.DBManager
 
-	//TODO related
-	manager.AutoMigrate(config.AutoMigrates...)
-
 	identity.CreateIdentityByForm(identity.FormIDToken{
 		AccountName:"likepc",
 		AccountPassword:"like#pc$0218",
 	}, manager)
-
-	// 기본값으로 만들고
-	// authenticator 를 초기화한다
-	ca := authenticator.NewMem(config.NodeNumber, config.SessionExpiresInSeconds)
-	ca.SetActions(identity.GetLoadCookieIDToken(manager),
-		identity.GetLoadIdentity(manager),
-		identity.GetPersistToken(manager))
-	r.Use(cthulthu.GinMiddleware(ca).Handler())
-	if len(config.AuthorizerConfig) > 0 {
-		authorizer.Init(r, config.AuthorizerConfig...)
-	}
-
-	r.Use(manager.Handler())
-	//TODO login redirect page 지정 필요
-	// renderer
-	if !helper.IsEmpty(config.TemplateDir) {
-		r.HTMLRender = render.New(config.TemplateDir)
-	}
 }
 
 func main() {
--- runner/runner.go
+++ runner/runner.go
@@ -2,9 +2,15 @@ package runner
 
 import (
 	"github.com/ezaurum/cthulthu/config"
-	"github.com/ezaurum/cthulthu/route"
 	"github.com/gin-gonic/gin"
 	"github.com/ezaurum/cthulthu/database"
+	"github.com/ezaurum/cthulthu/helper"
+	"github.com/ezaurum/cthulthu/render"
+	"github.com/ezaurum/cthulthu/route"
+	"github.com/ezaurum/cthulthu/authenticator"
+	"github.com/ezaurum/cthulthu/identity"
+	"github.com/ezaurum/cthulthu"
+	"github.com/ezaurum/cthulthu/authorizer"
 )
 
 const (
@@ -24,13 +30,34 @@ func Run(config *config.Config, addr...string) {
 	db := manager.Connect()
 	defer db.Close()
 
+	// DB 초기화
+	manager.AutoMigrate(config.AutoMigrates...)
+
 	config.DBManager = manager
 
 	r := gin.Default()
 
 	config.Initialize(r)
 
+	// authenticator 를 초기화한다
+	ca := authenticator.NewMem(config.NodeNumber, config.SessionExpiresInSeconds)
+	ca.SetActions(identity.GetLoadCookieIDToken(manager),
+		identity.GetLoadIdentity(manager),
+		identity.GetPersistToken(manager))
+	r.Use(cthulthu.GinMiddleware(ca).Handler())
+	if len(config.AuthorizerConfig) > 0 {
+		authorizer.Init(r, config.AuthorizerConfig...)
+	}
+
 	route.InitRoute(r, config.Routes...)
+
+	// DB 핸들러 설정
+	r.Use(manager.Handler())
+
+	// 템틀릿 렌더러 설정
+	if !helper.IsEmpty(config.TemplateDir) {
+		r.HTMLRender = render.New(config.TemplateDir)
+	}
 
 	r.Run(addr...)
 }
