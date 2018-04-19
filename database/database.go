7fbe6343949d66c3d7dae60c08b42f2fe22b44dc
조석규 <ezaurum@gmail.com>
2018-04-19 07:20:25 -0700

    Remove unused method

--- database/database.go
+++ database/database.go
@@ -3,18 +3,8 @@ package database
 import "time"
 
 type Model struct {
-	ID        int64 `gorm:"primary_key"`
+	ID        int64      `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
-}
-
-func (m *Model) BeforeUpdate() (err error) {
-	m.UpdatedAt = time.Now()
-	return
-}
-
-func (m *Model) BeforeCreate() (err error) {
-	m.CreatedAt = time.Now()
-	return
 }
