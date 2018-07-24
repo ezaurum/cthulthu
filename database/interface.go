177e1fd97822bd509d1533d078ebba2498a61f7d
조석규 <ezaurum@gmail.com>
2018-07-24 22:45:51 +0900

    Add util func

--- database/interface.go
+++ database/interface.go
@@ -17,3 +17,15 @@ type Entity interface {
 	ReadyBy(db *gorm.DB, where string, args ...interface{}) (bool, error)
 	Delete(db *gorm.DB) (bool, error)
 }
+
+func ReadBy(entity interface{}, db *gorm.DB, where string, args ...interface{}) (bool, error) {
+	r := db.Where(where, args...).Find(entity)
+	if r.Error != nil {
+		return false, r.Error
+	}
+	return true, nil
+}
+
+func Delete(entity interface{}, db *gorm.DB) {
+	db.Delete(entity)
+}
