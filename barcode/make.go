c9b2f7314fd49c1052468d4393da8247ad723753
조석규 <ezaurum@gmail.com>
2018-07-24 14:00:51 +0900

    Add barcode maker

---
+++ barcode/make.go
@@ -0,0 +1,53 @@
+package barcode
+
+import (
+	"image"
+	"github.com/boombuler/barcode/code128"
+	"image/draw"
+	cimage "github.com/ezaurum/cthulthu/image"
+)
+
+func MakeMMSBarCodeFile(codeString string, fileName string, defaultImage image.Image) (error, bool) {
+	cs, e := code128.Encode(codeString)
+	if nil != e {
+		return e, false
+	}
+
+	backgroundBounds := image.Rect(0, 0, 320, 480)
+	canvas := image.NewRGBA(backgroundBounds)
+
+	if nil != defaultImage {
+		defaultImage = cimage.Resize(defaultImage, 320, 480)
+	}
+
+	barCode := cimage.Resize(cs, 280, 100)
+
+	barCodeBounds := image.Rect(20, 370, 300, 480)
+	if nil != defaultImage {
+		draw.Draw(canvas, backgroundBounds, defaultImage, image.ZP, draw.Src)
+	}
+	draw.Draw(canvas, barCodeBounds, barCode, image.ZP, draw.Src)
+
+	cimage.CreateJPEG(fileName, canvas)
+
+	return nil, true
+}
+
+func MakeBarCodeFile(codeString string, fileName string) (error, bool) {
+	cs, e := code128.Encode(codeString)
+	if nil != e {
+		return e, false
+	}
+
+	backgroundBounds := image.Rect(0, 0, 220, 12)
+	canvas := image.NewRGBA(backgroundBounds)
+
+	barCode := cimage.Resize(cs, 200, 10)
+
+	barCodeBounds := image.Rect(10, 1, 210, 11)
+	draw.Draw(canvas, barCodeBounds, barCode, image.ZP, draw.Src)
+
+	cimage.CreateJPEG(fileName, canvas)
+
+	return nil, true
+}
