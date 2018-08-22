7b0fe205f6d01912f998b5a065d25c3a02763ebf
조석규 <ezaurum@gmail.com>
2018-08-22 21:43:37 +0900

    Add string in barcode

--- barcode/make.go
+++ barcode/make.go
@@ -7,9 +7,13 @@ import (
 	"github.com/skip2/go-qrcode"
 	"errors"
 	"fmt"
-	"image/color"
 	"github.com/boombuler/barcode/code128"
 	"github.com/boombuler/barcode"
+	"github.com/golang/freetype"
+	"io/ioutil"
+	"log"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
 )
 
 const (
@@ -19,40 +23,72 @@ const (
 	DefaultMMSHeight = 1138
 )
 
+var (
+	FontName = "luxisr.ttf"
+	Font     *truetype.Font
+)
+
+// initialize font
+func InitializeFont() {
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(FontName)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	Font = f
+}
+
 //640px by 1138px
-func MakeMMSBarCodeFile(codeString string, fileName string, defaultImage image.Image) (error, bool) {
-	cs, e := code128.Encode(codeString)
-	if nil != e {
-		return e, false
+func MakeMMSBarCodeFile(codeString string,
+	fileName string, defaultImage image.Image,
+	withCodeString bool) (error, bool) {
+
+	var barCode image.Image
+	var err error
+	if withCodeString {
+		barCode, err = MakeBarCodeWithString(codeString)
+	} else {
+		barCode, err = MakeBarCode(codeString)
+	}
+	if nil != err {
+		return err, false
 	}
 
 	var backgroundBounds image.Rectangle
 	var canvas draw.Image
 	if nil != defaultImage {
 		backgroundBounds = defaultImage.Bounds()
-		i := cs.Bounds().Dx() + ImagePadding
-		if backgroundBounds.Dx() < i {
-			return errors.New(fmt.Sprintf("default images is too small, need bigger then %v", i)), false
+		if backgroundBounds.Dx() < DefaultMMSWidth {
+			return errors.New(fmt.Sprintf("default images is too small, need bigger then %v", DefaultMMSWidth)), false
 		}
 		canvas = image.NewRGBA(backgroundBounds)
 	} else {
 		backgroundBounds = image.Rect(0, 0, DefaultMMSWidth, ImageHeight)
 		canvas = image.NewRGBA(backgroundBounds)
-	}
-
-	draw.Draw(canvas, canvas.Bounds(), &image.Uniform{C: color.White}, image.ZP, draw.Src)
-
-	barCode, err := barcode.Scale(cs, DefaultMMSWidth, ImageHeight)
-	if nil != err {
-		return err, false
+		draw.Draw(canvas, canvas.Bounds(), image.White, image.ZP, draw.Src)
 	}
 
 	barcodeWidth := barCode.Bounds().Dx()
 	paddingA := (backgroundBounds.Dx() - int(barcodeWidth)) / 2
 
 	maxPoint := backgroundBounds.Max
-	barCodeBounds := image.Rect(paddingA,
-		maxPoint.Y-ImageHeight, paddingA+barcodeWidth, maxPoint.Y)
+	var barCodeBounds image.Rectangle
+	if nil != defaultImage {
+		barCodeBounds = image.Rect(paddingA,
+			maxPoint.Y-ImageHeight-40, paddingA+barcodeWidth, maxPoint.Y-40)
+	} else {
+		barCodeBounds = image.Rect(paddingA,
+			maxPoint.Y-ImageHeight, paddingA+barcodeWidth, maxPoint.Y)
+	}
 
 	if nil != defaultImage {
 		draw.Draw(canvas, backgroundBounds, defaultImage, image.ZP, draw.Src)
@@ -80,13 +116,63 @@ func MakeBarCode(codeString string) (image.Image, error) {
 	if nil != e {
 		return nil, e
 	}
-	backgroundBounds := image.Rect(0, 0, 220, 12)
-	canvas := image.NewRGBA(backgroundBounds)
-	barCode := paint.Resize(cs, 200, 10)
-	barCodeBounds := image.Rect(10, 1, 210, 11)
-	draw.Draw(canvas, barCodeBounds, barCode, image.ZP, draw.Src)
 
-	return canvas, nil
+	barCode, err := barcode.Scale(cs, DefaultMMSWidth, ImageHeight)
+	if nil != err {
+		return nil, err
+	}
+
+	return barCode, nil
+}
+
+func MakeBarCodeWithString(codeString string) (image.Image, error) {
+
+	code, e := MakeBarCode(codeString)
+
+	if nil != e {
+		return code, e
+	}
+
+	bounds := code.Bounds()
+	paddingLeft := 10
+	clip := image.Rect(bounds.Min.X+paddingLeft, bounds.Max.Y-50, bounds.Max.X-paddingLeft, bounds.Max.Y)
+	canvas := image.NewRGBA(bounds)
+	draw.Draw(canvas, bounds, code, image.ZP, draw.Src)
+	draw.Draw(canvas, clip, image.White, image.ZP, draw.Src)
+
+	barCodeBounds := image.Rect(bounds.Min.X,
+		bounds.Min.Y, bounds.Max.X, bounds.Max.Y-40)
+
+	draw.Draw(canvas, barCodeBounds, code, image.ZP, draw.Src)
+
+	fontSize := 24.0
+	dpi := 72.0
+
+	c := freetype.NewContext()
+	c.SetDPI(dpi)
+	if nil == Font {
+		InitializeFont()
+	}
+	c.SetFont(Font)
+	c.SetFontSize(fontSize)
+	c.SetClip(clip)
+	c.SetDst(canvas)
+	c.SetSrc(image.Black)
+	hinting := "none"
+	switch hinting {
+	default:
+		c.SetHinting(font.HintingNone)
+	case "full":
+		c.SetHinting(font.HintingFull)
+	}
+
+	pt := freetype.Pt(paddingLeft*2, clip.Min.Y+int(c.PointToFixed(fontSize)>>6)+paddingLeft)
+	_, err := c.DrawString(codeString, pt)
+	if err != nil {
+		return canvas, err
+	}
+
+	return canvas, e
 }
 
 func MakeQR(url string, size int) image.Image {
--- barcode/make_test.go
+++ barcode/make_test.go
@@ -9,15 +9,41 @@ import (
 
 func TestMakeBarCode(t *testing.T) {
 	generate := snowflake.New(0).Generate()
-	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", nil)
+	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", nil, false)
 	assert.True(t, b)
 	assert.Nil(t, e)
 }
 
 func TestMakeBarCode2(t *testing.T) {
 	generate := snowflake.New(0).Generate()
-	jpeg := paint.LoadJPEG("dd.jpg")
-	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", jpeg)
+	jpeg := paint.LoadJPEG("wisdom_default.jpg")
+	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", jpeg, false)
+	assert.True(t, b)
+	assert.Nil(t, e)
+}
+
+func TestMakeBarCodeWithString(t *testing.T) {
+	generate := snowflake.New(0).Generate()
+	img, err := MakeBarCodeWithString(generate)
+
+	paint.CreateJPEG(generate+".jpg", img)
+
+	assert.NotNil(t, img)
+	assert.Nil(t, err)
+
+}
+
+func TestMakeBarCodeWithString2(t *testing.T) {
+	generate := snowflake.New(0).Generate()
+	e, b := MakeMMSBarCodeFile(generate, generate+"TestMakeBarCodeWithString2.jpg", nil, true)
+	assert.True(t, b)
+	assert.Nil(t, e)
+}
+
+func TestMakeBarCodeWithString3(t *testing.T) {
+	generate := snowflake.New(0).Generate()
+	jpeg := paint.LoadJPEG("wisdom_default.jpg")
+	e, b := MakeMMSBarCodeFile(generate, generate+"TestMakeBarCodeWithString3.jpg", jpeg, true)
 	assert.True(t, b)
 	assert.Nil(t, e)
 }
