396c271d410b6d75c635685c8026b0b71d4f565b
조석규 <ezaurum@gmail.com>
2018-11-19 22:58:53 +0900

    Update test

--- barcode/make_test.go
+++ barcode/make_test.go
@@ -1,6 +1,7 @@
 package barcode
 
 import (
+	"fmt"
 	"testing"
 	"github.com/ezaurum/cthulthu/generators/snowflake"
 	"github.com/stretchr/testify/assert"
@@ -8,14 +9,14 @@ import (
 )
 
 func TestMakeBarCode(t *testing.T) {
-	generate := snowflake.New(0).Generate()
+	generate := fmt.Sprintf("%X", snowflake.New(0).GenerateInt64())[3:]
 	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", nil, false)
 	assert.True(t, b)
 	assert.Nil(t, e)
 }
 
 func TestMakeBarCode2(t *testing.T) {
-	generate := snowflake.New(0).Generate()
+	generate := fmt.Sprintf("%X", snowflake.New(0).GenerateInt64())[3:]
 	jpeg := paint.LoadJPEG("wisdom_default.jpg")
 	e, b := MakeMMSBarCodeFile(generate, generate+".jpg", jpeg, false)
 	assert.True(t, b)
@@ -23,7 +24,7 @@ func TestMakeBarCode2(t *testing.T) {
 }
 
 func TestMakeBarCodeWithString(t *testing.T) {
-	generate := snowflake.New(0).Generate()
+	generate := fmt.Sprintf("%X", snowflake.New(0).GenerateInt64())[3:]
 	img, err := MakeBarCodeWithString(generate)
 
 	paint.CreateJPEG(generate+".jpg", img)
@@ -34,15 +35,15 @@ func TestMakeBarCodeWithString(t *testing.T) {
 }
 
 func TestMakeBarCodeWithString2(t *testing.T) {
-	generate := snowflake.New(0).Generate()
+	generate := fmt.Sprintf("%X", snowflake.New(0).GenerateInt64())[3:]
 	e, b := MakeMMSBarCodeFile(generate, generate+"TestMakeBarCodeWithString2.jpg", nil, true)
 	assert.True(t, b)
 	assert.Nil(t, e)
 }
 
 func TestMakeBarCodeWithString3(t *testing.T) {
-	generate := snowflake.New(0).Generate()
-	jpeg := paint.LoadJPEG("wisdom_default.jpg")
+	generate := fmt.Sprintf("%X", snowflake.New(0).GenerateInt64())[3:]
+	jpeg := paint.LoadJPEG("dd.jpg")
 	e, b := MakeMMSBarCodeFile(generate, generate+"TestMakeBarCodeWithString3.jpg", jpeg, true)
 	assert.True(t, b)
 	assert.Nil(t, e)
