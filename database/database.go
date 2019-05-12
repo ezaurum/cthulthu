b03ecf8688fab57a668d1dd31eb46c7e924110ae
조석규 <ezaurum@gmail.com>
2019-05-12 14:58:17 +0900

    Add deleted at to json

--- database/database.go
+++ database/database.go
@@ -10,5 +10,5 @@ type Model struct {
 	ID        int64      `gorm:"primary_key" json:"id,string"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `sql:"index" json:"-"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at,omitempty"`
 }
