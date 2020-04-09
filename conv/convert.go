e7dd80909b1583ccef973ce84d4ea7302c690131
조석규 <ezaurum@gmail.com>
2020-04-10 05:20:48 +0900

    feat: Add to base64

--- conv/convert.go
+++ conv/convert.go
@@ -1,6 +1,9 @@
 package conv
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"github.com/pkg/errors"
 	"strconv"
 	"strings"
 )
@@ -35,4 +38,22 @@ func ToFloat64(param string) (float64, bool) {
 		return 0, false
 	}
 	return id, true
+}
+
+func ToBase64Json(v interface{}) (string, error) {
+	marshal, err := json.Marshal(v)
+	if nil != err {
+		return "", errors.Wrapf(err, "marshal to json failed")
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(marshal)
+	return encoded, nil
+}
+
+func FromBase64Json(base64string string, v interface{}) error {
+	decodeString, err := base64.StdEncoding.DecodeString(base64string)
+	if nil != err {
+		return err
+	}
+	return json.Unmarshal(decodeString, v)
 }
