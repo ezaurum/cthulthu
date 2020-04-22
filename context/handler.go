2df14d396987ad388273a29ab93de47013da874d
조석규 <ezaurum@gmail.com>
2020-04-22 15:31:39 +0900

    feat: Add readonly handler

--- context/handler.go
+++ context/handler.go
@@ -19,14 +19,14 @@ type ResponseWriter interface {
 	Complete(c echo.Context) error
 }
 
+func HandlerError(r *Request, c echo.Context) {
+	c.JSON(http.StatusInternalServerError, r.Error)
+}
+
 // 세션 사용, 트랜잭션 사용
 func DefaultHandler(ctx Context, logicArray ...RequestHandlerFunc) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		repo := ctx.Repository()
-		writer := repo.Writer()
-		reader := repo.Reader()
-		// 컨텍스트 생성
-		r := NewWithDB(c, writer, reader)
+		r := newRequest(c, ctx)
 		r.ResultType = c.Request().Header.Get("Content-Type")
 
 		// 쿠키 읽기
@@ -66,6 +66,47 @@ func DefaultHandler(ctx Context, logicArray ...RequestHandlerFunc) func(c echo.C
 	}
 }
 
-func HandlerError(r *Request, c echo.Context) {
-	c.JSON(http.StatusInternalServerError, r.Error)
+// 세션 사용, 트랜잭션 미 사용
+func ReadOnlyHandler(ctx Context, logicArray ...RequestHandlerFunc) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		r := newRequest(c, ctx)
+		r.ResultType = c.Request().Header.Get("Content-Type")
+
+		// 쿠키 읽기
+		r.Cookie = cookie.New(c.Request(), c.Response())
+
+		scn := "session-cookie-name"
+		domain := "localhost"
+		// 세션 읽기
+		//todo
+		maxAge := 3600
+		r.LoadSession(scn, maxAge)
+
+		//todo 권한 처리
+		err := r.RunAll(logicArray)
+
+		// 결과 전송
+		// 에러 있는 경우
+		if nil != err {
+			HandlerError(r, c)
+			return nil
+		}
+
+		// 세션 쓰기 - 쿠키로
+		clientCookieName := "ss-cck"
+		r.SaveSession(scn, clientCookieName, domain)
+		// 쿠키 쓰기
+		r.Cookie.Write()
+
+		return r.Complete(c)
+	}
+}
+
+func newRequest(c echo.Context, ctx Context) *Request {
+	repo := ctx.Repository()
+	writer := repo.Writer()
+	reader := repo.Reader()
+	// 컨텍스트 생성
+	r := NewWithDB(c, writer, reader)
+	return r
 }
